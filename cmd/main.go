559974f8424cb33b97c9e6c6fa24d293dd344b61
LOG <log.plusplus@gmail.com>
2026-03-14 19:00:37 +0300

    cmd/main.go

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,10 @@ package main
 //version 2.4.1
 
 import (
+	"log"
+	"time"
 	"webtest/internal/database"
+	"webtest/internal/logger"
 	"webtest/internal/server"
 	"webtest/pkg/colors"
 	"webtest/pkg/intro"
@@ -20,6 +23,23 @@ func main() {
 	dataBase, dbVersion := database.ConnectDB()
 	defer dataBase.Close()
 
+	///////////////////////// <--- RabbitMQ TEST BEGIN
+	time.Sleep(3 * time.Second) //Ждем RabbitMQ
+	err := logger.NewMessage()
+	if err != nil {
+		return
+	}
+	log.Printf("---Сообщение отправлено---\n")
+
+	time.Sleep(500 * time.Millisecond)
+	err = logger.ReadMessage()
+	if err != nil {
+		return
+	}
+	log.Printf("---Сообщение прочтено---\n\n")
+	///////////////////////// <--- RabbitMQ TEST END
+
 	//Запускаем сервер
+	log.Printf("Сервер запущен!")
 	server.StartServer(&dbVersion)
 }
